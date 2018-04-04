31f1d0eb47f75b47880a71fedecc7f16d543656b
Mihir Garude <mgmihir44@users.noreply.github.com>
2018-04-04 17:11:53 -0400

    Create client.h

---
+++ client.h
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+
+int open_client_socket();
+void connect_to_server(int , int);
