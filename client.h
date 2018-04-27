bfa45673f46462d708f912e651e758e16fc59fba
Mihir Garude <mgmihir44@users.noreply.github.com>
2018-04-27 18:35:35 -0400

    Delete client.h

--- client.h
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <sys/socket.h>
-#include <arpa/inet.h>
-#include <unistd.h>
-
-int open_client_socket();
-void connect_to_server(int , int);
